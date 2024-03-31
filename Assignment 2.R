e7df2bdd12b143e8ba64451ab186c87ab2ad1324
Nisaysin <164195762+Nisaysin@users.noreply.github.com>
2024-03-31 15:50:38 +0200

    Update Assignment 2.R

--- Assignment 2.R
+++ Assignment 2.R
@@ -1,10 +1,10 @@
-#Task 1: Importing
+#### Task 1: Importing ####
 library(readr)
 appstore_games <- read.csv("appstore_games.csv")
 appstore_games <- data.frame(appstore_games)
 
 
-#Task 2: Data Cleaning
+#### Task 2: Data Cleaning ####
 library(dplyr)
 data<-appstore_games
 
@@ -18,7 +18,7 @@ clean_df$In.app.Purchases <- strsplit(clean_df$In.app.Purchases, ",")
 #count the number of in-app purchases per game
 clean_df$IAP_count <- sapply(clean_df$In.app.Purchases,FUN = length)
 
-#converting everything to numeric
+#converting In.app.Purchases to numeric
 clean_df$In.app.Purchases <- sapply(clean_df$In.app.Purchases,as.numeric)
 
 #convert 0 into NA value
@@ -28,7 +28,7 @@ for(c in 1:nrow(clean_df)) {
   }
 }
 
-#calcularing min IAP
+#calculating min IAP
 for(d in 1:nrow(clean_df)) {
   if (clean_df$IAP_count[d]==0) {
     clean_df$IAP_min[d] <- NA
@@ -48,7 +48,7 @@ for(d in 1:nrow(clean_df)) {
 
 #calculating the sum and the mean IAP
 clean_df$IAP_sum <- sapply(clean_df$In.app.Purchases,FUN =sum)
-clean_df$IAP_average <- sapply(clean_df$In.app.Purchases, FUN = function(x) round(mean(x), 2)) #Yasin - I changed it to two decimals
+clean_df$IAP_average <- sapply(clean_df$In.app.Purchases, FUN = function(x) round(mean(x), 2))
 
 #checking for NA and infinite values in Description
 sum(is.na(clean_df$Description))
@@ -74,8 +74,8 @@ table(clean_df$Age.Rating)
 clean_df$age_rating<-ifelse(clean_df$Age.Rating=="4+","4+","9+")
 
 
+#creating a new column for languages count
 clean_df$Languages<-strsplit(clean_df$Languages, ",")
-
 
 for(x in 1:nrow(clean_df)) {
   lang_length <- length(clean_df$Languages[[x]])
@@ -90,7 +90,7 @@ for(x in 1:nrow(clean_df)) {
 
 clean_df$is_available_in_english <- "No"
 
-#Note: we refer to another column for NAs
+#note: we refer to another column for NAs
 for(y in 1:nrow(clean_df)){
   if(is.na(clean_df$languages_count[[y]])){
     clean_df$is_available_in_english[y] <- NA
@@ -99,57 +99,51 @@ for(y in 1:nrow(clean_df)){
   }
 }
 
-#load the extra df
+#loading the extra df
 load("genres_df.Rda") 
 
-#create a table with totals for each ID 
+#creating a table with totals for each ID 
 Genres_ID <- table(Genres_df$ID)
-#print to check
+#printing to check
 print(Genres_df$ID)
-#match the ID from the table to the df and add the total genres to the df
+#matching the ID from the table to the df and adding the total genres to the df
 for(ID in names(Genres_ID)) {
   clean_df$Genres_count[clean_df$ID==ID] <- Genres_ID[ID]
 }
 
-#we still have the Primary.Genres attribute which we're not using
-#it's the first genre in the normal Genres column
-#we can remove the attribute together with the other 3 in the very beginning of task 2
-#should we do it?
-#Yasin - I don't think it is hindering the rest of the code, so I don't think we should remove it
-
 unique_genres <- unique(Genres_df$genre)
 print(unique_genres)
 
-#add coumns for all unique genres 
+#adding columns for all unique genres 
 for(genre in unique_genres) {
   clean_df [ ,genre] <- 0
 }
 
-#rows: match IDs from two data frames
-#columns: match the genre names in Genres_df to the column names in clean_df 
+#rows: matching IDs from two data frames
+#columns: matching the genre names in Genres_df to the column names in clean_df 
 for (e in 1:nrow(Genres_df)) {
   clean_df[clean_df$ID==Genres_df$ID[e],Genres_df$genre[e]] <- 1
 }
 
+#calculating elapsed months since release date and describing the release month
 clean_df$Current.Version.Release.Date <- as.Date(clean_df$Current.Version.Release.Date, format = "%d/%m/%Y")
-clean_df$Elapsed_months<- as.numeric(difftime(Sys.Date(),clean_df$Current.Version.Release.Date,units="days"))/30 #Yasin - I changed the name of the column to Elapsed_months and adjusted the code to have it return the correct number of months
-clean_df$Release_month<- months(as.Date(clean_df$Original.Release.Date)) #Andrea - Consider rounding the elapsed months
+clean_df$Elapsed_months<- as.numeric(difftime(Sys.Date(),clean_df$Current.Version.Release.Date,units="days"))/30 
+clean_df$Release_month<- months(as.Date(clean_df$Original.Release.Date)) 
 
-#free/non-free app
+#describing whether free/non-free app
 clean_df$game_free <- ifelse(is.na(clean_df$In.app.Purchases),1,0)
 
-#finding a median
+#finding a median for user rating count
 median_rating <- median(clean_df$User.Rating.Count, na.rm = TRUE)
 median_rating
 clean_df$Categorical.Rating.Count <- ifelse(clean_df$User.Rating.Count < median_rating, "Low", "High")
 
 
-####    TASK 3: Missing Values formatting ####
+#### Task 3: Missing Values Formatting ####
 
 clean_df_copy <- clean_df
 #From the paper, it is possible to read that games which show NA User.Rating.Count have not reached 5 total rating 
-#Replacing NA User.Rating.Count with 5
-
+#In the paper they changed the NA values to "5", but we are changing them to "0" as this is in accordance with assignment description
 clean_df_copy$User.Rating.Count <- ifelse(
   is.na(clean_df_copy$User.Rating.Count) , 0, clean_df_copy$User.Rating.Count
 )
@@ -162,10 +156,8 @@ clean_df_copy$Average.User.Rating <- ifelse(
 #Handling NA Price values 
 sum(is.na(clean_df_copy$Price))
 
-#Yasin - I changed this to replace NA values to 0, instead of removing the rows completely
 clean_df_copy <- clean_df_copy %>%
   mutate(Price = ifelse(is.na(Price), 0, Price))
-
 
 #Replacing NA in-app-purchases with 0 
 clean_df_copy$In.app.Purchases <- ifelse(
@@ -176,7 +168,6 @@ clean_df_copy$In.app.Purchases <- ifelse(
 clean_df_copy <- clean_df_copy %>%
   filter(lengths(Languages)>0)
 
-
 #To double check
 sum(clean_df_copy$Languages == '0')
 
@@ -185,7 +176,6 @@ clean_df_copy <- clean_df_copy %>%
   mutate(Size = ifelse(is.na(Size), 0, Size))
 
 #Converting IAP NA values measurements in 0 
-#Yasin - I replaced the code with an easier solution for this:
 clean_df_copy <- clean_df_copy %>%
   mutate(
     IAP_min = ifelse(is.na(IAP_min), 0, IAP_min),
@@ -200,18 +190,11 @@ clean_df_copy <- clean_df_copy%>%
 
 clean_df_copy <- clean_df_copy%>% 
   filter (!is.na(is_available_in_english))
-#Re-running rating median calculation after data imputation
-#median_rating <- median(clean_df_copy$User.Rating.Count, na.rm = TRUE)
-#edian_rating
-#clean_df_copy$Categorical.Rating.Count <- ifelse(clean_df_copy$User.Rating.Count < median_rating, "Low", "High")
-#clean_df_copy <- clean_df_copy%>% 
-#filter (!is.na(Categorical.Rating.Count))
 
 #Checking for NA values 
 sum(apply(is.na(clean_df_copy[,1:76] ), 2, sum))
 
-
-#### Making sure that every column is either numerical or categorical ####
+#Making sure that every column is either numerical or categorical
 column_types <- sapply(clean_df_copy,class)
 column_types
 table(column_types)
@@ -225,6 +208,7 @@ library(lubridate)
 
 #Original.Release.Date to numeric
 date_components <- lapply(clean_df_copy$Original.Release.Date, function(date_str) unlist(strsplit(date_str, "/")))
+
 #Creating a function that takes each component of a date (Year, month and day), transforms them as numeric and then returns the entire date as numeric
 numeric_dates <- sapply(date_components, function(components) {
   year <- as.numeric(components[1])
@@ -269,27 +253,25 @@ table(column_types)
 str(clean_df_copy)
 
 #Checking for duplicates 
-
 clean_df_copy_unique <- unique(clean_df_copy)
 str(clean_df_copy_unique)
 
-#Re-calculating Dependend variable, to remove Missing Values 
-
+#Re-calculating Dependent variable, to remove Missing Values 
 clean_df_copy_unique$Categorical.Rating.Count <- ifelse(clean_df_copy_unique$User.Rating.Count < median_rating, "Low", "High")
 table(clean_df_copy_unique$Categorical.Rating.Count)
 
-#### TASK 4 ####
+#### Task 4: Data Partitioning ####
 #Splitting dataset into training and testing set 
 install.packages('caTools')
 library(caTools)
 split <- sample.split(clean_df_copy_unique, SplitRatio = 0.8)
 
-#Creating Training and test sets 
+#Creating training and est sets 
 trainingSet <- subset(clean_df_copy_unique,split == TRUE)
 testSet <- subset(clean_df_copy_unique,split == FALSE)
 
 
-#### TASK 5 ####
+#### Task 5: Scaling Attributes ####
 #Select numeric columns
 numeric_columns <- sapply(clean_df_copy_unique, is.numeric)
 
@@ -311,11 +293,11 @@ testSet[, numeric_columns_testSet] <- lapply(testSet[, numeric_columns_testSet],
   (x - min(x, na.rm = TRUE)) / (max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
 })
 
-#### TASK 6 ####
+#### Task 6: Evaluation  ####
 
 # PREP
 
-# remove Games and Strategy columns 
+# removing Games and Strategy columns 
 # the values do not contribute to the outcome of the algorithms
 trainingSet$Games <- NULL
 trainingSet$Strategy <- NULL
@@ -407,11 +389,3 @@ pred
 cm <- table(testing$Categorical.Rating.Count, pred)
 cm
 confusionMatrix(cm)
-
-# for some reason,the prediction wasn't made for all observations in the testing set
-# you can see it by running these lines
-length(pred)
-(testing)
-
-
-
